0ef8fb428b79f9adcf6b3c5571ebeed718f38abc
Mihaela Simion <mihaelasimion2016@gmail.com>
2020-05-08 13:28:03 +0300

    Initial Commit

---
+++ EasyToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyToDo
+//
+//  Created by Mihaela Simion on 5/8/20.
+//  Copyright © 2020 Mihaela Simion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
